1c09b2565c856c80d0fb3abd3106efa8caf7e3b6
Hitarth Choubisa <hitarthc64@gmail.com>
2020-11-11 21:49:18 -0500

    Update segments.py

--- roost/segments.py
+++ roost/segments.py
@@ -193,7 +193,7 @@ class ResidualNetwork(nn.Module):
         )
         self.acts = nn.ModuleList([activation() for _ in range(len(dims) - 1)])
 
-        self.fc_out = nn.Linear(dims[-1], output_dim)
+        self.fc_out = nn.Linear(dims[-1], int(output_dim))
 
     def forward(self, x):
         for fc, bn, res_fc, act in zip(self.fcs, self.bns,
